87b5532177e48213c58e80bce7243172163934f9
Mary <silvaa.marianyy@gmail.com>
2025-01-17 10:42:49 -0300

    Alterações no banco de dados

--- inventario/models.py
+++ inventario/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -11,5 +12,11 @@ class   Inventario(models.Model):
     marca=models.CharField(max_length=50,null=True)
     estado_de_uso=models.CharField(max_length=50,null=False)
     imagem=models.CharField(max_length=100,null=True)
+    
+class Usuario(models.Model):
+    matricula=models.CharField(max_length=15,null=False)    
+    campus=models.CharField(max_length=50,null=False)
+    status=models.CharField(max_length=50,null=False)
+    user=models.ForeignKey(User,on_delete=models.CASCADE)
     
     
