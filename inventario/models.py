c6e48475ec95a0495cb5f20585ad5050e1eefc28
Mary <silvaa.marianyy@gmail.com>
2025-01-24 08:25:53 -0300

    Atualização do Banco de Dados

--- inventario/models.py
+++ inventario/models.py
@@ -2,8 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-
-class   Inventario(models.Model):
+class Campus(models.Model): 
+    campus=models.CharField(max_length=50,null=False)
+     
+class Inventario(models.Model):
     nome=models.CharField(max_length=50,null=False)
     validade=models.DateField(null=True)
     quantidade=models.CharField(max_length=50,null=False)
@@ -12,10 +14,11 @@ class   Inventario(models.Model):
     marca=models.CharField(max_length=50,null=True)
     estado_de_uso=models.CharField(max_length=50,null=False)
     imagem=models.CharField(max_length=100,null=True)
+    campus=models.ForeignKey(Campus, on_delete=models.CASCADE)
     
 class Usuario(models.Model):
     matricula=models.CharField(max_length=15,null=False)    
-    campus=models.CharField(max_length=50,null=False)
+    campus=models.ForeignKey(Campus, on_delete=models.CASCADE)
     status=models.CharField(max_length=50,null=False)
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     
